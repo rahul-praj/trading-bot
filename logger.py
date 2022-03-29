32ad1b13624d504ce98990e0ec709cb104599c37
rahul-praj <102345745+rahul-praj@users.noreply.github.com>
2022-03-29 23:22:10 +1100

    Logging script

--- logger.py
+++ logger.py
@@ -2,8 +2,26 @@
 # -*- coding: utf-8 -*-
 
 import logging as lg
+import os
+from datetime import datetime
 
-lg.basicConfig(filename='trading_bot.log', format = '%(asctime)s: %(message)s', level = lg.DEBUG)
+# create folder for logging
+path = './logs'
+try:
+    os.mkdir(path)
+except:
+    print('Creation of directory %s failed' % path)
+
+# rename each log depending on the timeout
+
+current = datetime.now().stfrtime("%Y%m%d_%H:%M:%S")
+log_name = current + '.log'
+currentLog_path = logs_path + log_name
+
+# log parameters
+
+lg.basicConfig(filename=log_name, format = '%(asctime)s: %(message)s', level = lg.DEBUG)
+lg.getLogger().addHandler(lg.StreamHandler())
 
 # logging levels: DEBUG, INFO, WARNING, ERROR
 
