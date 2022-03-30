95fb004ec8bc811c0cf2e9aa1b5d928d6f471116
rahul-praj <102345745+rahul-praj@users.noreply.github.com>
2022-03-30 18:56:35 +1100

    Update logger.py

--- logger.py
+++ logger.py
@@ -8,22 +8,22 @@ from datetime import datetime
 def initialize_logger():
 
 
-# create folder for logging
-path = './logs' # define the path
-try:
-    os.mkdir(path)
-except:
-    print('Creation of directory %s failed' % path)
+    # create folder for logging
+    path = './logs' # define the path
+    try:
+        os.mkdir(path)
+    except:
+        print('Creation of directory %s failed' % path)
 
-# rename each log depending on the timeout
+    # rename each log depending on the timeout
 
-current = datetime.now().stfrtime("%Y%m%d_%H:%M:%S")
-log_name = current + '.log'
-currentLog_path = logs_path + log_name
+    current = datetime.now().stfrtime("%Y%m%d_%H:%M:%S")
+    log_name = current + '.log'
+    currentLog_path = logs_path + log_name
 
-# log parameters
+    # log parameters
 
-lg.basicConfig(filename=log_name, format = '%(asctime)s: %(message)s', level = lg.DEBUG)
-lg.getLogger().addHandler(lg.StreamHandler())
+    lg.basicConfig(filename=log_name, format = '%(asctime)s: %(message)s', level = lg.DEBUG)
+    lg.getLogger().addHandler(lg.StreamHandler())
 
-lg.info('Log initialized')
+    lg.info('Log initialized')
