849404985e0304bc4330a90de76b3b010d6236d3
rahul-praj <102345745+rahul-praj@users.noreply.github.com>
2022-03-30 18:53:03 +1100

    Update bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+# import needed libraries
+from trading_bot import *
+from logger import *
+# initialize the logger
+initialize_logger
+
+# check my trading account
+
+# close current orders
+
+# define assets
+    # IN: keyboard
+    # OUT:  string
+
+# execute trading bot
+    # IN: string (ticker)
+    # OUT: boolean (True = success / False = failure)
