9f9d97cf39fbfcef4ecd7054ee5839469916709b
rahul-praj <102345745+rahul-praj@users.noreply.github.com>
2022-03-30 19:27:41 +1100

    Update logger.py

--- logger.py
+++ logger.py
@@ -17,7 +17,7 @@ def initialize_logger():
 
     # rename each log depending on the timeout
 
-    current = datetime.now().strftime("%Y%m%d_%H:%M:%S")
+    current = datetime.now().strftime("%Y%m%d_%H%M%S")
     log_name = current + '.log'
     currentLog_path = logs_path + log_name
 
