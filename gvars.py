2842348b14397e0ad107db53103d4cdd4db6dc98
rahul-praj <102345745+rahul-praj@users.noreply.github.com>
2022-04-12 23:19:26 +1000

    Create gvars.py

---
+++ gvars.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+stopLossMargin = 0.05 # percentage margin for stop stop loss
+
+takeProfitMargin = 0.1 # percentage margin for the take profit
+
+maxSpentEquity = 1000 # total equity to spend in a single operation
+
+# MAX ATTEMPTS SECTION
+
+maxAttemptsCP = 5 # check position
+
+maxAttemptsGCP = 5 # GET CURRENT PRICE
+
+sleepTimeCP = 5 # CHECK POSITION TIME
+sleepTimeGCP = 5 # GET CURRENT PRICE TIME
